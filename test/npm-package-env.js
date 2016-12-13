eff06dece5d0c8c89e2e5efb8676c93904625e94
eliranm <eliranm@liveperson.com>
2016-12-13 18:32:43 +0200

    add tests, fix prefix reset

---
+++ test/npm-package-env.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*
+ * this test must be run as an npm script for the library to work properly
+ * 
+ * the fixtures are actually the package.json fields, so the root package.json is used for that,
+ * and the "config" field is there solely for that purpose.
+ */
+
+const assert = require('assert');
+
+
+describe('npm-package-env', function () {
+
+    const env = require('../lib/npm-package-env');
+    
+    beforeEach(function init() {
+        env._.at('npm_package_config');
+    });
+
+    describe('#string', function () {
+
+        it('should return the property value', function () {
+            let result = env.foo._.string();
+            assert.equal(result, 'bar');
+        });
+
+    });
+
+    describe('#object', function () {
+
+        it('should return an object with values specified by keys', function () {
+            let result = env.obj._.object(['foo']);
+            console.log(`result: ${JSON.stringify(result)}`);
+            assert.equal(result.foo, 'bar');
+        });
+
+    });
+
+    xdescribe('when called with dot accessor', function () {
+    });
+
+});
